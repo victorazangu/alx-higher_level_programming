5615d521c1c26e3e242593e87696ce39dd13cb71
victorazangu <victorsazangu@gmail.com>
2022-06-04 03:36:18 -0700

    corrections

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
+
 def element_at(my_list, idx):
-    for i in my_list:
-        if idx < 0:
-            return None
-        elif idx > len(my_list):
-            return None
-        else:
-            return my_list[idx]
+    """ Returns an element in a liust"""
+    if idx < 0 or idx > len(my_list) - 1:
+        return None
+    else:
+        return my_list[idx]
