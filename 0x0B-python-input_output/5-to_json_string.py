9a644235a51fe3452d07e4897fa60832ca38f8b6
victorazangu <victorsazangu@gmail.com>
2022-07-05 21:01:39 +0300

    corrections

--- 0x0B-python-input_output/5-to_json_string.py
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
-from json import dumps
+"""Module 5-to_json_string.
+Returns the JSON representation of an object.
+"""
+
+
+import json
 
 
 def to_json_string(my_obj):
-    return dumps(my_obj)
+    """Returns th JSON representation of my_obj.
+
+    Args:
+        - my_obj: string to represent
+
+    Returns: JSON representation
+    """
+
+    return json.dumps(my_obj)
